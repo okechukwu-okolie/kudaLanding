a45dedc078cf54b0054363c468e3d43523876a2e
okechukwu samuel okolie <okechukwu.okolie.2023@gmail.com>
2024-11-12 12:21:58 -0800

    get request with loading status

--- src/pages/GetRequest.jsx
+++ src/pages/GetRequest.jsx
@@ -1,61 +1,56 @@
-import styled from 'styled-components'
+
 import { useEffect, useState } from 'react'
-// // import React from 'react';
-// import Backdrop from '@mui/material/Backdrop';
-// import CircularProgress from '@mui/material/CircularProgress';
-// import Button from '@mui/material/Button';
+
 
 
 import React from 'react'
 
 const GetRequest = () => {
 
-    const [blogPost, setBlogPost]=useState([])
+    const [blogPost, setBlogPost]=useState([]);
+    const [display,setDisplay]=useState(true);
 
     useEffect(()=>{
-        //fetch data from api
+        
         fetch('https://jsonplaceholder.typicode.com/posts')
        .then(response => response.json())
-       .then(data=>setBlogPost(data))
-    // .then(data=>{console.log(data)})
-    },[])
+       .then((data)=>{
+        setBlogPost(data);
+        setDisplay(false);
+    })
+        .catch((error)=>{
+            console.error('Fetching post error',error);
+            setDisplay(false)
+        })
+},[])
 
 
 
   return (
-    <Container>
+    <div>
         <div>
-            <ul>{blogPost.map((blog,index) =>{return <p key={index}>{blog.title} <br /><small>{blog.body}</small></p>})}
-            </ul>
-        </div>
-        {/* <Backdrop sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
-        open>
-            <CircularProgress color="inherit" />
-            </Backdrop> */}
-    </Container>
+            {display ? (
+            <div>
+                <img 
+                src="https://i.gifer.com/4V0b.gif" 
+                alt="Retrieving posts........." 
+                style={{width:'600px',height:'600px',marginLeft:'300px',marginBottom:'700px'}}
+                />
+                <p>Waiting...................</p>
+            </div>
+            ):(
+            <div>
+                <h2>Blog Post</h2>
+                <ul>{blogPost.map((blog)=>(
+                    <li key={blog.id}>
+                        <h3>{blog.title}</h3>
+                        <p>{blog.body}</p>
+                    </li>
+                ))}</ul>
+            </div>)}
+              </div>
+    </div>
   )
 }
 
 export default GetRequest
-const Container = styled.div`
-text-align: center;
-margin-top: 50px;
-
-ul{
-    /* padding-bottom: 130px; */
-    width: 80%;
-    margin:0 auto;
-    text-align: center;
-}
-small{
-    color: red;
-}
-p{
-    /* background-color: grey; */
-    /* max-width: 400px; */
-    margin-bottom: 50px;
-    border: 1px solid black;
-    padding: 20px;
-    border-radius: 30px;
-}
-`
